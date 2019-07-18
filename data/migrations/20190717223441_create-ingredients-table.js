485f3bc1ccdc2136a3de1fac406b5bb1732e90b8
alexisjcarr <acarr59@gmail.com>
2019-07-17 22:40:46 -0500

    ➕ Adds create-ingredients-table migration
     #1

---
+++ data/migrations/20190717223441_create-ingredients-table.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("ingredients", tbl => {
+    tbl.increments();
+    tbl.string("ingredient_name", 255).notNullable();
+    tbl
+      .float("measurement")
+      .notNullable()
+      .unsigned();
+    tbl
+      .integer("recipe_id")
+      .notNullable()
+      .unsigned();
+    tbl.foreign("recipe_id").references("recipes.id");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("ingredients");
+};
+
+/*id, name, measurement (floating number), recipe_id */
