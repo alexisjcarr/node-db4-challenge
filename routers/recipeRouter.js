1d19eb831130fa48e0b9491bfe3e76a471aec9f6
alexisjcarr <acarr59@gmail.com>
2019-07-17 22:03:25 -0500

    ➕ Adds middleare folder.
     #1

--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
 
+router.get("/", async (req, res) => {});
+
+router.get("/:id/shoppingList", async (req, res) => {});
+
+router.get("/:id/instructions", async (req, res) => {});
+
+router.get("/:id/recipes", async (req, res) => {});
+
 module.exports = router;
